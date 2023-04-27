04977665c58c79b3263ce0fab4c9cf0fd3a73898
Hu Chia Wei <kenneth_hu@livemail.tw>
2023-04-28 06:25:08 +0800

    add ERC721 project and MyERC721

---
+++ Code/MyERC721.sol
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: MIT
+// CHECK https://wizard.openzeppelin.com/#erc721 to generate / customise your own ERC721 contract
+pragma solidity >=0.6.0 <0.9.0;
+
+
+import "@openzeppelin/contracts@4.5.0/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.5.0/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts@4.5.0/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts@4.5.0/security/Pausable.sol";
+import "@openzeppelin/contracts@4.5.0/access/Ownable.sol";
+import "@openzeppelin/contracts@4.5.0/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts@4.5.0/utils/cryptography/draft-EIP712.sol";
+import "@openzeppelin/contracts@4.5.0/token/ERC721/extensions/draft-ERC721Votes.sol";
+
+
+contract MyERC721 is ERC721, ERC721Enumerable, ERC721URIStorage, Pausable, Ownable, ERC721Burnable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor(string memory name, string memory symbol) ERC721(name, symbol) {}
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function safeMint(address to, string memory uri) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        whenNotPaused
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _afterTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721)
+    {
+        super._afterTokenTransfer(from, to, tokenId);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
+
---
+++ Code/NethereumERC721/NethereumERC721/Program.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Nethereum.Web3;
+using Nethereum.Web3.Accounts;
+using Nethereum.StandardNonFungibleTokenERC721;
+using Nethereum.StandardNonFungibleTokenERC721.ContractDefinition;
+using Nethereum.ABI.FunctionEncoding.Attributes;
+using Nethereum.Contracts;
+using System.Numerics;
+
+
+namespace NethereumERC721
+{
+
+  
+    class Program
+    {
+        static   void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // deployERC721().Wait();
+            // MintingERC721().Wait();
+            // BalanceAndOwnerERC721().Wait();
+             BalanceAndOwner2ERC721().Wait();
+             TransferFrom2ERC721().Wait();
+             BalanceAndOwner2ERC721().Wait();
+      
+        }
+
+        [Function("safeTransferFrom")]
+       public class SafeTransferFromFucntion : FunctionMessage{
+         [Parameter("address","from",1)]
+            public string From{get; set;}
+
+             [Parameter("address","to",2)]
+            public string To{get; set;}
+          
+            [Parameter("uint256","tokenId",3)]
+            public BigInteger TokenId{get; set;}
+
+       }
+
+         static async Task TransferFrom2ERC721(){
+            var privatekey = "";
+
+            var account = new Account(privatekey);
+
+            var web3 = new Web3(account, "HTTP://127.0.0.1:8545");
+  
+            var safeTransferFromMessage = new SafeTransferFromFucntion(){
+                From="0x5375E7270d796dCD6c4048809539D3CC04476c32",
+                To="0x796838555b8C3730377b008aA0cC7a164d1A9276",
+                TokenId=1
+            };
+
+            var contractHandler = web3.Eth.GetContractHandler("0xF1dd6A4F045dDAf300269aA556d74Ef8EbFb5B1B");
+
+            var result  =  await contractHandler.SendRequestAsync(safeTransferFromMessage);
+
+            Console.WriteLine("result : " + result);
+          
+       } 
+    
+           static async Task TransferFromERC721(){
+            var privatekey = "";
+
+            var account = new Account(privatekey);
+
+            var web3 = new Web3(account, "HTTP://127.0.0.1:8545");
+
+            var tokenService = new ERC721Service(web3,"0xF1dd6A4F045dDAf300269aA556d74Ef8EbFb5B1B");
+
+            var result = await tokenService.TransferFromRequestAndWaitForReceiptAsync("0x796838555b8C3730377b008aA0cC7a164d1A9276","0x5375E7270d796dCD6c4048809539D3CC04476c32", 1, new CancellationTokenSource());
+        
+            Console.WriteLine("result : " + result);
+            
+       } 
+      
+         static async Task BalanceAndOwner2ERC721(){
+            var privatekey = "";
+
+            var account = new Account(privatekey);
+
+            var web3 = new Web3(account, "HTTP://127.0.0.1:8545");
+  
+            var ownerOfMessage = new OwnerOfFucntion(){TokenId=1};
+            var queryHandler = web3.Eth.GetContractQueryHandler<OwnerOfFucntion>();
+
+            var owner  =  await queryHandler.QueryAsync<string>("0xF1dd6A4F045dDAf300269aA556d74Ef8EbFb5B1B", ownerOfMessage).ConfigureAwait(false);
+
+            var balanceOfMessage = new BalanceOfFucntion(){Owner = owner};
+
+            var queryHanlder2 = web3.Eth.GetContractQueryHandler<BalanceOfFucntion>();
+
+            var balance = await queryHanlder2.QueryAsync<BigInteger>("0xF1dd6A4F045dDAf300269aA556d74Ef8EbFb5B1B", balanceOfMessage);
+
+        // 0xF1dd6A4F045dDAf300269aA556d74Ef8EbFb5B1B
+            Console.WriteLine("owner : " + owner);
+             Console.WriteLine("balance : " + balance);
+       } 
+    
+        
+       [Function("ownerOf","address")]
+       public class OwnerOfFucntion : FunctionMessage{
+          
+            [Parameter("uint256","tokenId",1)]
+            public BigInteger TokenId{get; set;}
+
+       }
+
+       
+       [Function("balanceOf", "uint256")]
+       public class BalanceOfFucntion : FunctionMessage{
+            [Parameter("address","owner",1)]
+            public string Owner{get; set;}
+
+       }
+
+
+         static async Task BalanceAndOwnerERC721(){
+            var privatekey = "";
+
+            var account = new Account(privatekey);
+
+            var web3 = new Web3(account, "HTTP://127.0.0.1:8545");
+
+            var tokenService = new ERC721Service(web3,"0xF1dd6A4F045dDAf300269aA556d74Ef8EbFb5B1B");
+
+            var owner = await tokenService.OwnerOfQueryAsync(1);
+            var balance = await tokenService.BalanceOfQueryAsync(owner);
+        // 0xF1dd6A4F045dDAf300269aA556d74Ef8EbFb5B1B
+            Console.WriteLine("owner : " + owner);
+             Console.WriteLine("balance : " + balance);
+
+       } 
+    
+      
+       static async Task deployERC721(){
+        var privatekey = "";
+
+        var account = new Account(privatekey);
+
+        var web3 = new Web3(account, "HTTP://127.0.0.1:8545");
+
+        var deploymentContract = new ERC721Deployment();
+
+        var token = await ERC721Service.DeployContractAndWaitForReceiptAsync(web3, deploymentContract);
+
+        Console.WriteLine("Contract Address : " + token.ContractAddress);
+       } 
+    
+       [Function("safeMint")]
+       public class SafeMintFucntion : FunctionMessage{
+            [Parameter("address","to",1)]
+            public string To{get; set;}
+
+            [Parameter("string","uri",2)]
+            public string Uri{get; set;}
+
+       }
+       static async Task MintingERC721(){
+            var privatekey = "";
+
+            var account = new Account(privatekey);
+
+            var web3 = new Web3(account, "HTTP://127.0.0.1:8545");
+
+            var safeMintFun = new SafeMintFucntion(){
+                To = account.Address,
+                Uri ="ipfs://QmdYeDpkVZedk1mkGodjNmF35UNxwafhFLVvsHrWgJoz6A/beanz_metadata/1"
+            };
+
+        // 0xF1dd6A4F045dDAf300269aA556d74Ef8EbFb5B1B
+
+            var contractHandler = web3.Eth.GetContractHandler("0xF1dd6A4F045dDAf300269aA556d74Ef8EbFb5B1B");
+            var result = await contractHandler.SendRequestAsync(safeMintFun);
+
+
+            Console.WriteLine("result : " + result);
+       } 
+    
+
+    }
+}
